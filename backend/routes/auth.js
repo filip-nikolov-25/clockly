4e65ef15ca31fa165a394ad7df1b2943f504c6ca
Filip Nikolov <filip.nikolov1010@gmail.com>
2026-03-26 05:05:14 +0100

    fix: backend missmatch with column with db

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -72,7 +72,7 @@ router.post("/register", async (req, res) => {
         .json({ message: "Invite code is required for employees" });
     }
     const inviteQuery = await pool.query(
-      "SELECT * FROM company_invites WHERE code = $1 AND used_by IS NULL",
+      "SELECT * FROM company_invites WHERE code = $1 AND created_by IS NULL",
       [code],
     );
     if (inviteQuery.rows.length === 0) {
@@ -100,7 +100,7 @@ router.post("/register", async (req, res) => {
 
   if (code) {
     await pool.query(
-      "UPDATE company_invites SET used_by = $1, used_at = NOW() WHERE code = $2",
+      "UPDATE company_invites SET created_by = $1, used_at = NOW() WHERE code = $2",
       [newUser.rows[0].id, code],
     );
   }
