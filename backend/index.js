a663f9ee8d8ded2abe9f5fd39331f247410d397a
Filip Nikolov <filip.nikolov1010@gmail.com>
2026-05-03 15:24:40 +0200

    testing

--- backend/index.js
+++ backend/index.js
@@ -23,19 +23,19 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
+    origin: (origin, callback) => {
       if (!origin) return callback(null, true);
 
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"));
+      console.log("Blocked CORS origin:", origin);
+      return callback(null, false);
     },
     credentials: true,
   })
 );
-
 
 app.use(express.json());
 app.use(cookieParser());
